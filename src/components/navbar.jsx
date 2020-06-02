fe566187a2ceef1486d30050b5fc53d4d3996071
austinckohler <austinckohler@gmail.com>
2020-06-02 14:16:52 -0600

    testing hooks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//stateless functional component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light m-auto ">
+      <a
+        className="navbar-brand navbar-text center"
+        href="https://unsplash.com/s/photos/el-chalt%C3%A9n"
+      >
+        Pictures of El Chatén on Unsplash{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
